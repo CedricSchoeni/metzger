c9488e2eae93190e437ce2aa3e96a96bd390b030
Campiotti <adriano.campiotti@gmail.com>
2018-01-27 17:02:07 +0100

    added Footer links

--- view/layout/footer.php
+++ view/layout/footer.php
@@ -13,10 +13,10 @@
     <div class="container_12">
         <div class="grid_12">
             <div class="socials">
-                <a href="#"></a>
-                <a href="#"></a>
-                <a href="#"></a>
-                <a href="#"></a>
+                <a href="https://www.facebook.com/NESRI.DISCOUNT/?hc_ref=PAGES_TIMELINE"></a>
+                <a href="https://twitter.com/nesridiscount"></a>
+                <a href="https://www.pinterest.fr/NesriDiscount/"></a>
+                <a href="https://plus.google.com/102227333673129621016"></a>
             </div>
             <div class="copy">
                 NesriDiscount &copy; 2018 | <a href="#">Privacy Policy</a>
