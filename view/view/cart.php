6c9d3740fe22472a3f6096127f8c942a1cff0a0d
Your Name <you@example.com>
2018-01-31 11:03:56 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	assets/css/main.css§

--- view/view/cart.php
+++ view/view/cart.php
@@ -5,26 +5,31 @@
  * Date: 31.01.2018
  * Time: 09:08
  */
-$product = $this->product[0];
+$cartItems = $this->cart;
 ?>
 
 <div class="content">
     <div class="container_12">
-
+        <h3 class="pb1"><span>Shopping Cart</span></h3>
+        <?php if ($cartItems[0]['id']) foreach($cartItems as $cart){?>
         <div class="grid_12">
-            <h3 class="pb1"><span>Shopping Cart</span></h3>
+
             <div class="grid_2"></div>
             <div class="grid_4"><img src="<?php echo$image?>" alt="product_image" class="img_inner fleft"></div>
             <div class="grid_4">
-                <div class="title"><?php echo$product['productname']?></div>
+                <div class="title"><?php echo$cart['productname']?></div>
                 <ul class="list l1">
-                    <li>Product Owner: <?php echo$product['username']?></li>
-                    <li>Price: <?php echo$product['price']?></li>
-                    <li>Stock: <?php echo$product['stock']?></li>
+                    <li>Product Owner: <?php echo$cart['username']?></li>
+                    <li>Price: <?php echo$cart['price']?></li>
+                    <li>Stock: <?php echo$cart['stock']?></li>
                 </ul>
             </div>
         </div>
         <div class="clear"></div>
+        <?php } else {?>
+            <div class="grid_12 text1 center">Your cart is empty</div>
+
+        <?php }?>
         </div>
     </div>
 </div>
