e6899920d40c0dfd1d6f4f9e4b18adfc4495481a
Campiotti <adriano.campiotti@gmail.com>
2018-02-01 18:09:20 +0100

    clear of products finally finished. {does work 100%}

--- view/view/products.php
+++ view/view/products.php
@@ -35,7 +35,6 @@
             $image=$noImg;
             if($products[$i]['Image']!=null)
                 $image=$path.$products[$i]['Image'];
-
             ?>
             <div class="grid_4">
                 <a href="<?php echo$image?>" class="gal"><img src="<?php echo$image?>" alt=""></a>
@@ -43,7 +42,8 @@
                 <div class="wordBreak"><?php echo$products[$i]["Description"]?></div><br>
                 <a href="/shop/product/<?php echo$products[$i]['ID']?>">Go to Product Details</a>
             </div>
-            <?php if($i>1 && 3%$i-1==0)echo"<div class='clear'></div>";} ?>
+            <?php if($i>1 && ($i+1)%3==0)echo"<div class='clear'>$i</div>
+";} ?>
 
     </div>
   </div>
