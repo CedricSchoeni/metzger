3374c48220fb5b5450a6ca1768d1fc695ff42887
Your Name <you@example.com>
2018-02-01 11:01:41 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	assets/css/main.css§

--- helper/HTMLHelper.php
+++ helper/HTMLHelper.php
@@ -10,7 +10,8 @@ namespace helper;
 
 class HTMLHelper extends BaseHelper
 {
-    public function formatPrice($price, $discount){
-        return ($discount > 0) ? '<span class="oldPrice">'.number_format((float)(round($price * 2, 1) / 2), 2, '.', '').'$</span><span class="newPrice">'.number_format((float)(round($price*(1 - $discount) * 2, 1) / 2), 2, '.', '').'$</span>' : '<span class="price">'.number_format((float)(round($price * 2, 1) / 2), 2, '.', '').'$</span>';
+    public function formatPrice($price, $discount, $id = ''){
+        //return ($discount > 0) ? $price : 'xdd';
+        return ($discount > 0) ? '<span class="oldPrice">'.number_format((float)(round($price * 2, 1) / 2), 2, '.', '').'$</span><span id="price'.$id.'" class="newPrice">'.number_format((float)(round($price*(1 - $discount) * 2, 1) / 2), 2, '.', '').'$</span>' : '<span id="price'.$id.'" class="price">'.number_format((float)(round($price * 2, 1) / 2), 2, '.', '').'$</span>';
     }
 }
